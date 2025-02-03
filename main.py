c40158f5581ae894cb9f4b431fb30c01dec752a0
manishadara04 <manishadara04@gmail.com>
2025-02-03 18:43:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,134 @@
+import os
+from pyrogram import Client as AFK, idle
+from pyrogram.enums import ChatMemberStatus, ChatMembersFilter
+from pyrogram import enums
+from pyrogram.types import ChatMember
+import asyncio
+import logging
+import tgcrypto
+from pyromod import listen
+from tglogging import TelegramLogHandler
+from flask import Flask, jsonify
+import threading
+
+# Config 
+class Config(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "23442913"))
+    API_HASH = os.environ.get("API_HASH", "864a97e16b4ff7dc65ff5e2d1549b4a2")
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    SESSIONS = "./SESSIONS"
+
+    AUTH_USERS = os.environ.get('AUTH_USERS', '6428531614').split(',')
+    for i in range(len(AUTH_USERS)):
+        AUTH_USERS[i] = int(AUTH_USERS[i])
+
+    GROUPS = os.environ.get('GROUPS', '-1002482220356').split(',')
+    for i in range(len(GROUPS)):
+        GROUPS[i] = int(GROUPS[i])
+
+    LOG_CH = -1002447298131
+
+# TelegramLogHandler is a custom handler which is inherited from an existing handler. ie, StreamHandler.
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        TelegramLogHandler(
+            token=Config.BOT_TOKEN, 
+            log_chat_id= Config.LOG_CH, 
+            update_interval=2, 
+            minimum_lines=1, 
+            pending_logs=200000),
+        logging.StreamHandler()
+    ]
+)
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.info("live log streaming to telegram.")
+
+
+# Store
+class Store(object):
+    CPTOKEN = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6ODU0ODUyMzIsIm9yZ0lkIjo2MzA0OCwidHlwZSI6MSwibW9iaWxlIjoiOTE5OTI1ODU1MzE2IiwibmFtZSI6IlBBTktBSiBCQVJJQSIsImVtYWlsIjoicGJhcmlhMzE4QGdtYWls[...]"
+    SPROUT_URL = "https://discuss.oliveboard.in/"
+    ADDA_TOKEN = ""
+    THUMB_URL = "https://telegra.ph/file/1f9bb59089588344bfb1f.jpg"
+
+# Format
+class Msg(object):
+    START_MSG = "**Running !!**"
+
+    TXT_MSG = "Hey <b>{user},"\
+        "\n\n`I'm Multi-Talented Robot. I Can Download Many Type of Links.`"\
+            "\n\nSend a TXT or HTML file :-</b>"
+
+    ERROR_MSG = "<b>DL Failed ({no_of_files}) :-</b> "\
+        "\n\n<b>Name: </b>{file_name},\n<b>Link:</b> `{file_link}`\n\n<b>Error:</b> {error}"
+
+    SHOW_MSG = "<b>Downloading :- "\
+        "\n`{file_name}`\n\nLink :- `{file_link}`</b>"
+
+    CMD_MSG_1 = "`{txt}`\n\n**Total Links in File are :-** {no_of_links}\n\n**Send any Index From `[ 1 - {no_of_links} ]` :-**"
+    CMD_MSG_2 = "<b>Uploading :- </b> `{file_name}`"
+    RESTART_MSG = "✅ HI Bhai log\n✅ PATH CLEARED"
+
+# Prefixes
+prefixes = ["/", "~", "?", "!", "."]
+
+# Health Check Server using Flask
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify(status='OK'), 200
+
+def run_health_check_server():
+    app.run(host='0.0.0.0', port=8082)  
+    
+# Start Health Check Server in a separate thread
+health_check_thread = threading.Thread(target=run_health_check_server)
+health_check_thread.daemon = True
+health_check_thread.start()
+
+# Client
+plugins = dict(root="plugins")
+if __name__ == "__main__":
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    if not os.path.isdir(Config.SESSIONS):
+        os.makedirs(Config.SESSIONS)
+
+    PRO = AFK(
+        "AFK-DL",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        sleep_threshold=120,
+        plugins=plugins,
+        workdir= f"{Config.SESSIONS}/",
+        workers= 2,
+    )
+
+    chat_id = []
+    for i, j in zip(Config.GROUPS, Config.AUTH_USERS):
+        chat_id.append(i)
+        chat_id.append(j)
+    
+    
+    async def main():
+        await PRO.start()
+        bot_info = await PRO.get_me()
+        LOGGER.info(f"<--- @{bot_info.username} Started --->")
+        
+        for i in chat_id:
+            try:
+                await PRO.send_message(chat_id=i, text="**Bot Started! ♾**")
+            except Exception as d:
+                print(d)
+                continue
+        await idle()
+
+    asyncio.get_event_loop().run_until_complete(main())
+    LOGGER.info(f"<---Bot Stopped--->")
