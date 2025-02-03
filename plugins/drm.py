a8fa4deff0404b00b7b1f39a0d5740a213b9f736
manishadara04 <manishadara04@gmail.com>
2025-02-03 20:44:05 +0530

    Update drm.py

--- plugins/drm.py
+++ plugins/drm.py
@@ -1,8 +1,16 @@
+from fileinput import filename
 from pyrogram import filters, Client as ace
+from main import LOGGER, prefixes
 from pyrogram.types import Message
-from main import LOGGER, prefixes, Config
+from main import Config
 import os
+import subprocess
+import tgcrypto
 import shutil
+import sys
+from handlers.uploader import Upload_to_Tg
+from handlers.tg import TgClient
+
 
 @ace.on_message(
     (filters.chat(Config.GROUPS) | filters.chat(Config.AUTH_USERS)) &
@@ -15,26 +23,6 @@ async def drm(bot: ace, m: Message):
 
     inputData = await bot.ask(m.chat.id, "**Send**\n\nMPD\nNAME\nQUALITY\nCAPTION")
     mpd, raw_name, Q, CP = inputData.text.split("\n")
-    await process_drm(bot, m, mpd, raw_name, Q, CP, path, tPath)
-
-@ace.on_message(
-    (filters.chat(Config.GROUPS) | filters.chat(Config.AUTH_USERS)) &
-    filters.incoming & filters.command("sendtxt", prefixes=prefixes)
-)
-async def sendtxt(bot: ace, m: Message):
-    msg = await bot.ask(m.chat.id, "Please upload the text file you want to send.")
-    
-    if msg.document and msg.document.mime_type == "text/plain":
-        file_path = await msg.download()
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            mpd, raw_name, Q, CP = [line.strip() for line in lines[:4]]
-            await process_drm(bot, m, mpd, raw_name, Q, CP, Config.DOWNLOAD_LOCATION, f"{Config.DOWNLOAD_LOCATION}/THUMB")
-        os.remove(file_path)
-    else:
-        await m.reply_text("Please upload a valid text file (with .txt extension).")
-
-async def process_drm(bot, m, mpd, raw_name, Q, CP, path, tPath):
     name = f"{TgClient.parse_name(raw_name)} ({Q}p)"
     print(mpd, name, Q)
 
@@ -67,13 +55,16 @@ async def process_drm(bot, m, mpd, raw_name, Q, CP, path, tPath):
                 os.system(cmd3)
                 os.remove(f'{path}/{data}')
 
+
         cmd4 = f'ffmpeg -i "{path}/video.mp4" -i "{path}/audio.m4a" -c copy "{path}/{name}.mp4"'
         os.system(cmd4)
         os.remove(f"{path}/video.mp4")
         os.remove(f"{path}/audio.m4a")
         filename = f"{path}/{name}.mp4"
         cc = f"{name}.mp4\n\n**Description:-**\n{CP}"
-        UL = Upload_to_Tg(bot=bot, m=m, file_path=filename, name=name, Thumb=Thumb, path=path, show_msg=prog, caption=cc)
+        # await DownUP.sendVideo(bot, m, filename, cc, Thumb, name, prog, path)
+        UL = Upload_to_Tg(bot=bot, m=m, file_path=filename, name=name,
+                            Thumb=Thumb, path=path, show_msg=prog, caption=cc)
         await UL.upload_video()
         print("Done")
     except Exception as e:
